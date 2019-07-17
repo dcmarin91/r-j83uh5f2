4d210d16710fa4edfb77c1f4aa4b2a78e18b762a
dcmarin91 <dcmarin91@hotmail.com>
2019-07-16 20:40:18 -0500

    Correct letter counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.increment = this.increment.bind(this);
+
+    this.state = { value: 0 }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.increment}></textarea>
+        <div className="counter">{this.state.value}</div>
       </div>
     );
+  }
+  increment(ev) {
+    this.setState({ value: ev.target.value.length});
   }
 }
 
